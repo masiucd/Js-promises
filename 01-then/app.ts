cc948d8d7dcc4f3d432929a32e5d5d8f09078dad
Marcell Ciszek <marcell.ciszek@etraveligroup.com>
2021-03-02 08:48:19 +0100

    feat:  starwars api

--- 01-then/app.ts
+++ 01-then/app.ts
@@ -1,16 +1,31 @@
-import axios from "axios"
-
-const getUsers = (URL: string, endPoint: string) => {
-  const response = axios.get(URL + endPoint).then(res => res.data) as Promise<
-    Array<any>
-  >
-  return response
+const getApiUrl = () => {
+  return "https://swapi.dev/api/"
 }
 
-const render = async (element: HTMLDivElement, URL: string) => {
-  const users = await getUsers(URL, "/users")
-  const output = users.map(user => ` <p>${user.name}</p>`).join("")
-  element.innerHTML = output
+const getData = (url: string) => (endPoint: string) => (fn: Function) => {
+  return fetch(url + endPoint)
+    .then(res => res.json())
+    .then(data => fn(data))
 }
 
-export { getUsers, render }
+const starWarsApi = getData(getApiUrl())
+const luke = starWarsApi("people/1")
+
+let x = luke((r: any) => console.log(r))
+
+// console.log(x)
+
+// // import axios from "axios"
+
+// const getUsers = (URL: string, endPoint: string) => {
+//   const response = axios.get(URL + endPoint).then(res => res.data) as Promise<Array<any>>
+//   return response
+// }
+
+// const render = async (element: HTMLDivElement, URL: string) => {
+//   const users = await getUsers(URL, "/users")
+//   const output = users.map(user => ` <p>${user.name}</p>`).join("")
+//   element.innerHTML = output
+// }
+
+// export { getUsers, render }
